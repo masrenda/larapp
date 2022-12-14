c9d7a98bc8b5add026a011b2e9102a27cc9e56e7
masrenda <tarenadiyaksa@gmail.com>
2022-12-14 08:34:46 +0700

    rmb update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,11 +15,11 @@ class PostController extends Controller
         ]);
     }
 
-    public function show($slug)
+    public function show(Post $post)
     {
         return view('posts', [
             "title" => "Single Post",
-            "blogPost" => Post::find($slug)
+            "blogPost" => $post
         ]);
     }
 }
